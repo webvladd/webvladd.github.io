771006cf84025b285ab83ac7f68ca1877d33c088
bilchenko <vb@tenviz.com>
2020-02-12 12:41:50 +0200

    form-sign-in-to-tenviz event

--- tenviz-landing-page/js/main.js
+++ tenviz-landing-page/js/main.js
@@ -1,5 +1,35 @@
 $(document).on('ready', function() {
 	console.log('test');
+	//***DOM ELEMENTS****
+	let $landingPageContainer = $('body');
+	console.log($landingPageContainer)
+	let $lpHeaderNav = $landingPageContainer.find('.lp-header-nav');
+	console.log($lpHeaderNav)
+	let $lpHeaderNavLogin = $lpHeaderNav.find('.lp-header-nav-login');
+	console.log($lpHeaderNavLogin)
+
+
+	let $formSignTenviz = $landingPageContainer.find('.form-sign-in-to-tenviz-section');
+	console.log($formSignTenviz)
+	let $formSignTenvizBtnClose = $formSignTenviz.find('.form-sign-in-to-tenviz-btn-close');
+	console.log($formSignTenvizBtnClose)
+	//***END DOM ELEMENTS****
+
+	//******EVENT********
+
+	//***form-sign-in-to-tenviz event***
+	$lpHeaderNavLogin.click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		$formSignTenviz.removeClass('hidden');
+	});
+
+	$formSignTenvizBtnClose.click(function(e) {
+		$formSignTenviz.addClass('hidden');
+	})
+	//***end form-sign-in-to-tenviz event***
+
+	//******END EVENT********
 
 	//*****Smooth scrolling to the anchor*****
 	var $page = $('html, body');
